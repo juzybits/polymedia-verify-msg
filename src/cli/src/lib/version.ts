e94620de63b404dd5570449651fc98eb9ea183a0
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-25 15:30:45 -0100

    cleanup

--- src/cli/src/lib/version.ts
+++ src/cli/src/lib/version.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 export function getVersion(): string {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
-    const packageText = readFileSync(join(__dirname, "../..", "package.json"), "utf8");
+    const packageText = readFileSync(join(__dirname, "..", "..", "package.json"), "utf8");
     const packageJson = JSON.parse(packageText); // eslint-disable-line
     return packageJson.version; // eslint-disable-line
 }
--- src/cli/src/verify.ts
+++ src/cli/src/verify.ts
@@ -30,18 +30,13 @@ const { message, address, signature } = program.opts<CommandOptions>();
         const _pubKey = await verifyPersonalMessageSignature(
             new TextEncoder().encode(message),
             signature,
-            { address }
+            { address },
         );
         console.log(JSON.stringify({
             success: true,
         }));
         process.exit(0);
     } catch (error) {
-        // - "Signature is not valid for the provided address"
-        // - "Signature is not valid for the provided message"
-        // - "Unsupported signature scheme"
-        // - "The string to be decoded is not correctly encoded"
-        // - "Unsupported signature scheme"
         console.error(JSON.stringify({
             success: false,
             error: error instanceof Error ? error.message : String(error),
